1afe035d79e07b1367e86b37a5dcb2b2c0eaa22a
Vishnu Prakash K <vishnuprakash71298@gmail.com>
2025-10-13 10:46:57 +0530

    Replace the old api endpoint to avoid the cache issue

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,6 +1,5 @@
-export const PRODUCTS_URL = `https://script.google.com/macros/s/AKfycbz3Ivl15FfACQcei_gfuHE7LB-iqW1eusCxepzl5Hq44uW6ROREpdwOpV048Sbt3e6A/exec?sheet=products`;
-export const CATEGORIES_URL = `https://script.google.com/macros/s/AKfycbz3Ivl15FfACQcei_gfuHE7LB-iqW1eusCxepzl5Hq44uW6ROREpdwOpV048Sbt3e6A/exec?`;
-export const FEATURED_PRODUCTS_URL = `https://script.google.com/macros/s/AKfycbwoX23d_uxE5brVKm3KZTXRzlM4Pq4PBAv9DIdDI_dPITtkcsTfROfitO65t4pKC1AN/exec?sheet=features`;
+export const PRODUCTS_URL = `https://script.google.com/macros/s/AKfycbwoX23d_uxE5brVKm3KZTXRzlM4Pq4PBAv9DIdDI_dPITtkcsTfROfitO65t4pKC1AN/exec?sheet=products`;
+export const CATEGORIES_URL = `https://script.google.com/macros/s/AKfycbwoX23d_uxE5brVKm3KZTXRzlM4Pq4PBAv9DIdDI_dPITtkcsTfROfitO65t4pKC1AN/exec?sheet=categories`;export const FEATURED_PRODUCTS_URL = `https://script.google.com/macros/s/AKfycbwoX23d_uxE5brVKm3KZTXRzlM4Pq4PBAv9DIdDI_dPITtkcsTfROfitO65t4pKC1AN/exec?sheet=features`;
 export const FEEDBACK_SENDER_URL = `https://script.google.com/macros/s/AKfycbwOB3uuPMh2R8tvY9Vrrg2bxxeCvZhGXJONWfBEn9LMGDaVSczHPYBHDmVU88VHWMXc/exec`;
 export const WHATSAPP_NUMBER = '9342727735';
 export const EMAIL_ID = 'ktrcycleworld006@gmail.com'
